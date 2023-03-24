70f0fe1c0f13649d43174cc01f00873ee632687b
eskiltorset <44784426+eskiltorset@users.noreply.github.com>
2023-03-24 15:34:03 +0100

    added shopping cart

---
+++ script/app2.js
@@ -0,0 +1,103 @@
+// select elements
+const cartItemsElement = document.querySelector(".cart-items");
+const subtotalElement = document.querySelector(".subtotal");
+
+//cart array
+let cart = JSON.parse(localStorage.getItem("cart"));
+updateCart();
+
+function addToCart(id) {
+  if (cart.some((item) => item.id === id)) {
+    changeNumberOfUnits("plus", id);
+  }
+  else {
+    const item = products.find((product) => product.id === id);
+
+    cart.push({
+      ...item,
+      numberOfUnits: 1,
+    });
+
+  }
+
+  updateCart();
+}
+
+ //update cart
+  function updateCart(){
+    renderCartItems();
+    renderSubtotal();
+
+    // save cart to localstorage
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  // calculate subtotal
+  function renderSubtotal(){
+    let totalPrice = 0;
+    let totalItems = 0;
+
+    cart.forEach((item) => {
+      totalPrice += item.price * item.numberOfUnits;
+      totalItems += item.numberOfUnits;
+    })
+
+    subtotalElement.innerHTML = `Subtotal (${totalItems} items): ${totalPrice},-`
+  }
+
+  //render cart items
+  function renderCartItems(){
+    cartItemsElement.innerHTML = "";
+    cart.forEach((item) => {
+      cartItemsElement.innerHTML += `
+                <div class="cart-item">
+                    <div class="item-info" onclick="removeItemFromCart(${item.id})">
+                        <img src="${item.imgSrc}" alt="${item.name}">
+                        <h4>${item.name}</h4>
+                    </div>
+                    <div class="unit-price">
+                        <small>$</small>${item.price}
+                    </div>
+                    <div class="units">
+                        <div class="btn minus" onclick="changeNumberOfUnits('minus', ${item.id})">-</div>
+                        <div class="number">${item.numberOfUnits}</div>
+                        <div class="btn plus" onclick="changeNumberOfUnits('plus', ${item.id})">+</div>           
+                    </div>
+                </div>
+      `
+    });
+  }
+
+  // remove item from cart
+  function removeItemFromCart(id){
+    cart = cart.filter((item) => item.id !== id);
+
+    updateCart();
+  }
+
+  //change number of units for an item
+  function changeNumberOfUnits(action, id){
+    cart = cart.map((item) => {
+
+      let numberOfUnits = item.numberOfUnits;
+
+      if(item.id === id){
+        if(action === "minus"){
+          numberOfUnits--;
+        } else if(action === "plus"){
+          numberOfUnits++;
+        }
+      }
+
+      return {
+        ...item,
+        numberOfUnits,
+      };
+    });
+
+    updateCart();
+  }
+
+  updateCart();
+
+  
---
+++ script/products.js
@@ -0,0 +1,100 @@
+const products = [
+    {
+      id: 0,
+      name: "1st V2 Alpine Jacket",
+      color: "Gray/Orange",
+      price: 1999,
+      description:
+        "Perfect for cold weather. Thick material ensures a warm body no matter the conditions. Popular for expeditions.",
+      imgSrc: "images/Image4.jpg",
+      numberOfUnits: 1,
+    },
+    {
+      id: 1,
+      name: "Softshell Jacket",
+      color: "Gray",
+      price: 899,
+      description:
+        "Perfect for cold weather. Thick material ensures a warm body no matter the conditions. Popular for expeditions.",
+      imgSrc: "images/Image00001.jpg",
+    },
+    {
+      id: 2,
+      name: "Skiing Jacket",
+      color: "Red/Black",
+      price: 799,
+      description:
+        "Perfect for cold weather. Thick material ensures a warm body no matter the conditions. Popular for expeditions.",
+      imgSrc: "images/Image6.jpg",
+    },
+    {
+      id: 3,
+      name: "Motorcycle Jacket",
+      color: "Gray",
+      price: 1399,
+      description:
+        "Perfect for cold weather. Thick material ensures a warm body no matter the conditions. Popular for expeditions.",
+      imgSrc: "images/Image9.jpg",
+    },
+    {
+      id: 4,
+      name: "Hiking Jacket",
+      color: "Gray",
+      price: 1299,
+      description:
+        "Perfect for cold weather. Thick material ensures a warm body no matter the conditions. Popular for expeditions.",
+      imgSrc: "images/Image8.jpg",
+    },
+    {
+      id: 5,
+      name: "Mountain Jacket V2",
+      color: "Black/White",
+      price: 1799,
+      description:
+        "Perfect for cold weather. Thick material ensures a warm body no matter the conditions. Popular for expeditions.",
+      imgSrc: "images/Image7.jpg",
+    },
+    {
+      id: 6,
+      name: "Fleece Jacket",
+      color: "Green",
+      price: 849,
+      description:
+        "Perfect for cold weather. Thick material ensures a warm body no matter the conditions. Popular for expeditions.",
+      imgSrc: "images/Image00002.jpg",
+    },
+  ];
+
+
+
+/*function passValues() {
+    let title = document.getElementById("jacket1-title").innerHTML;
+    let color = document.getElementById("jacket1-color").innerHTML;
+    let price = document.getElementById("jacket1-price").innerHTML;
+    let image = document.getElementById("jacket1-img").src;
+
+    localStorage.setItem("title", title);
+    localStorage.setItem("color", color);
+    localStorage.setItem("price", price);
+    localStorage.setItem("image", image);
+
+    console.log(localStorage);
+}
+
+function getValues() {
+    
+    let image1 = localStorage.getItem("image");
+    let title1 = localStorage.getItem("title");
+    let price1 = localStorage.getItem("price");
+    let color1 = localStorage.getItem("color");
+
+    document.getElementsByClassName("left-content-product").innerHTML = 
+        `<img src="${image1}" alt="Jacket 1" class="center">`;
+    
+    document.getElementsByClassName("right-content-product").innerHTML = 
+        `<h1>${title1}</h1>
+        <p>${color1}</p>
+        <h4 class="price">${price1}</h4>
+        <p class="product-description">Perfect for cold weather. Thick material ensures a warm body no matter 
+            the conditions. Popular for expeditions.</p>`;
+}*/
